efea46d99d3acfeec65ed5145f059e1e3f4b98f8
Brad Jones <brad@bjc.id.au>
2020-03-18 18:41:20 +1100

    fix(binary): make use of our new packages dexeca and dexecve
    This was always the intention but as a side effect of this change it should resolve an issue where StdErr was being buffered and displayed out of order.

--- bin/drun.dart
+++ bin/drun.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
+import 'package:dexeca/dexeca.dart';
+import 'package:dexecve/dexecve.dart';
 
 const _version = '0.0.0-semantically-released';
 
-Future main(List<String> args) async {
+Future main(List<String> argv) async {
   final script = 'Makefile.dart';
   final file = File(script);
 
   if (!await file.exists()) {
-    if (args.contains('--version') || args.contains('-v')) {
+    if (argv.contains('--version') || argv.contains('-v')) {
       stdout.writeln(_version);
       exit(0);
     }
@@ -17,8 +19,19 @@ Future main(List<String> args) async {
     exit(1);
   }
 
-  final proc = await Process.start('dart', <String>[script, ...args]);
-  await stdout.addStream(proc.stdout);
-  await stderr.addStream(proc.stderr);
-  exit(await proc.exitCode);
+  var args = <String>[script, ...argv];
+
+  if (Platform.isWindows) {
+    final result = await dexeca(
+      'dart',
+      args,
+      captureOutput: false,
+    );
+    exit(result.exitCode);
+  }
+
+  dexecve('dart', args);
+
+  stderr.writeln('dexecve failed to start dart ${args}');
+  exit(1);
 }
